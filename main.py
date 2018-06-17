2ce1c6645a9c716a4c6a86c6d4c3f0211019753d
ironfinger <tomhoughton888@gmail.com>
2018-06-17 15:41:13 +0100

    Remove Http for now

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ from HttpData import HttpData
 from WeatherDataPoints import WeatherDataPoints
 
 def main():
-    http = HttpData()
-    data = http.GET('https://api.darksky.net/forecast/668bf60cf034c2c299111995b6d32d81/51.5074,0.1278', 'json')
     dataPoint = WeatherDataPoints()
     dataPoint.clear_day()
 
