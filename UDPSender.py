076c91c3f6fb2d803160ae24424cc08ad092eabe
ironfinger <tomhoughton888@gmail.com>
2018-06-17 15:14:47 +0100

    Made snder

--- UDPSender.py
+++ UDPSender.py
@@ -4,13 +4,12 @@ import sys
 def sender():
     msgFromClient = "Hello it's Tom"
     bytesToSend = str.encode(msgFromClient)
-    serverAddressPort = ("", "11000")
+    serverAddressPort = ("192.168.1.207", 11000)
     bufferSize = 1024
 
-    UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+    UDPClientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-    UDPClientSocket.sendto(bytesToSend, serverAddressPort)
+    UDPClientSocket.connect(serverAddressPort)
 
-    msgFromServer = UDPClientSocket.recvfrom(bufferSize)
-
-    msg = "Message from server {}",format(msgFromServer[0])
+    UDPClientSocket.send(msgFromClient.encode('utf-8'))
+sender()
